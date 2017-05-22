87f54a352e4b1533401538377e980651dbc51b80
HughMungusHorse <kavi1989@gmail.com>
2017-05-22 11:43:36 +0200

    FB-knap added i footer
    ^

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 <footer>
     
 
-    <p class="text-center">OLDEBOLLE</p>
-
+  <a class="facebookknap" href="https://www.facebook.com/Trygsen-Service-Udlejning-203150059699908/?fref=ts">
+    <i class="fa fa-facebook-official fa-5" aria-hidden="true"></i></a>
+    
 </footer>
 </body>
 
