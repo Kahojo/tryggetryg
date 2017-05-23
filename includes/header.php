ecdba7cc6243e5887a0516e767e2531feaeaf814
manulul <eaaemnie1@students.eaaa.dk>
2017-05-23 14:24:05 +0200

    Rettet stavefejl i title
    Fra serice til service

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    <title>Trygsen Serice og Udlejning</title>
+    <title>Trygsen Service og Udlejning</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.css">
