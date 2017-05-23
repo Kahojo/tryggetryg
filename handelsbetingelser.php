7c4ba39318931ee418f9c5392812c10ade281333
HughMungusHorse <kavi1989@gmail.com>
2017-05-23 20:43:13 +0200

    Last commit
    Fikset de sidste dele:
    Fuldt opdateret sql
    Rettet små HTML fejl

--- handelsbetingelser.php
+++ handelsbetingelser.php
@@ -1,65 +1,55 @@
 <?php include 'includes/header.php' ?>
 <section class="container-fluid">
-<section class="col-sm-4"></section>
-    <section class="col-sm-4 faq"><h1>Handelsbetingelser</h1>
+    <section class="col-sm-4"></section>
+    <section class="col-sm-4 faq">
+        <h1>Handelsbetingelser</h1>
         <br>
         <h3>Betaling</h3>
         <p>
-        FIRMANAVN modtager betaling med Dankort/VISA-Dankort, VISA, VISA Electron, Mastercard og JCB. Betaling vil først blive trukket på din konto, når varen afsendes.
-        <br>
-        Alle beløb er i DKK. Danske kroner og er incl. moms.
-        <br>
-        FIRMANAVN bruger en godkendt betalingsserver, der krypterer alle dine kortoplysninger med SSL (Secure Socket Layer) protokol. Det betyder, at man ikke kan aflæse dine informationer.
+            FIRMANAVN modtager betaling med Dankort/VISA-Dankort, VISA, VISA Electron, Mastercard og JCB. Betaling vil først blive trukket på din konto, når varen afsendes.
+            <br> Alle beløb er i DKK. Danske kroner og er incl. moms.
+            <br> FIRMANAVN bruger en godkendt betalingsserver, der krypterer alle dine kortoplysninger med SSL (Secure Socket Layer) protokol. Det betyder, at man ikke kan aflæse dine informationer.
         </p>
-        <br><hr>
+        <br>
+        <hr>
         <h3>Levering</h3>
         <p>FIRMANAVN leverer på alle hverdage, med ordre indgået inden kl. 12.00
+            <br> NOTE: Her skrives jeres egne leveringsformer og betingelser!</p>
         <br>
-            NOTE: Her skrives jeres egne leveringsformer og betingelser!</p>
-        <br><hr>
+        <hr>
         <h3>Reklamationsret</h3>
         <p>Der gives 2 års reklamationsret i henhold til købeloven. Vores reklamationsret gælder for fejl i materiale og/eller fabrikation.
-        <br>
-        Reklamationen gælder ikke fejl eller skader begået ved forkert betjening af produktet / ydelsen.
-        <br>
-        FIRMANAVN vil dække returneringsomkostninger i et rimeligt omfang.
-        <br>
-        Ved returnering
-        Rettes henvendelse til og reklamationer sendes til:
-        <br>
-        FIRMANAVN
-        ADRESSEVEJ 4
-        9999 BYNAVN
-        <br>
-        Der modtages ikke pakker sendt på efterkrav.
-        <br><hr>
-        <h4>Refusion</h4><p>
-        Hvis der er aftalt refusion, så bedes I sende jeres bankoplysninger, så vi kan overføre det aftalte beløb.
-        </p><br><hr>
-        <h3>Fortrydelsesret</h3>
-        <p>Der gives 14 dages fuld returret på varer købt i vores Webshop.
-        Perioden regnes fra den dag, hvor du modtager ordren.
-        <br>
-        Returneringsomkostninger skal du selv afholde.
-        <br>
-        Fortrydelse skal anmeldes til os senest 14 efter købet og fra fortrydelsen skal I senest 14 dage efter returnere forsendelsen.
-        <br>
-        Meddelelsen skal gives pr. mail på info@domæne.dk. I meddelelsen skal du gøre os tydeligt opmærksom på, at du ønsker at udnytte din fortrydelsesret. Du kan også vælge at benytte vores standard fortrydelsesformular og sende med retur. Den finder du her (INDSÆT LINK TIL FORTRYDELSESFORMULAR)
-        <br><hr>
-        <h3>Varens stand, når du sender den retur</h3>
-        <p>Du hæfter kun for eventuel forringelse af varens værdi, som skyldes anden håndtering, end hvad der er nødvendigt for at fastslå varens art, egenskaber og den måde, den fungerer på. Med andre ord – du kan prøve varen på samme måde, som hvis du prøvede den i en fysisk butik.
-        <br>
-        Hvis varen er prøvet udover, hvad der er beskrevet ovenfor, betragter vi den som brugt, hvilket betyder, at du ved fortrydelse af købet kun får en del eller intet af købsbeløbet retur, afhængig af varens handelsmæssige værdi.
-        <br>
-        For at modtage hele købsbeløbet retur må du altså gøre det samme, som man kan i en fysisk butik. Du må afprøve varen, men ikke tage den i egentlig brug.
-        <br></p>
-       
-    
-    
-    
-    
+            <br> Reklamationen gælder ikke fejl eller skader begået ved forkert betjening af produktet / ydelsen.
+            <br> FIRMANAVN vil dække returneringsomkostninger i et rimeligt omfang.
+            <br> Ved returnering Rettes henvendelse til og reklamationer sendes til:
+            <br> FIRMANAVN ADRESSEVEJ 4 9999 BYNAVN
+            <br> Der modtages ikke pakker sendt på efterkrav.
+            <br>
+            <hr>
+            <h3>Refusion</h3>
+            <p>
+                Hvis der er aftalt refusion, så bedes I sende jeres bankoplysninger, så vi kan overføre det aftalte beløb.
+            </p><br>
+            <hr>
+            <h3>Fortrydelsesret</h3>
+            <p>Der gives 14 dages fuld returret på varer købt i vores Webshop. Perioden regnes fra den dag, hvor du modtager ordren.
+                <br> Returneringsomkostninger skal du selv afholde.
+                <br> Fortrydelse skal anmeldes til os senest 14 efter købet og fra fortrydelsen skal I senest 14 dage efter returnere forsendelsen.
+                <br> Meddelelsen skal gives pr. mail på info@domæne.dk. I meddelelsen skal du gøre os tydeligt opmærksom på, at du ønsker at udnytte din fortrydelsesret. Du kan også vælge at benytte vores standard fortrydelsesformular og sende med retur. Den finder du her (INDSÆT LINK TIL FORTRYDELSESFORMULAR)
+                <br>
+                <hr>
+                <h3>Varens stand, når du sender den retur</h3>
+                <p>Du hæfter kun for eventuel forringelse af varens værdi, som skyldes anden håndtering, end hvad der er nødvendigt for at fastslå varens art, egenskaber og den måde, den fungerer på. Med andre ord – du kan prøve varen på samme måde, som hvis du prøvede den i en fysisk butik.
+                    <br> Hvis varen er prøvet udover, hvad der er beskrevet ovenfor, betragter vi den som brugt, hvilket betyder, at du ved fortrydelse af købet kun får en del eller intet af købsbeløbet retur, afhængig af varens handelsmæssige værdi.
+                    <br> For at modtage hele købsbeløbet retur må du altså gøre det samme, som man kan i en fysisk butik. Du må afprøve varen, men ikke tage den i egentlig brug.
+                    <br></p>
+
+
+
+
+
     </section>
-<section class="col-sm-4"></section>
+    <section class="col-sm-4"></section>
 
 
 
